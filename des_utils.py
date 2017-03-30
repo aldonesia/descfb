7b83e06b302d72108dd3a150f60c15233e607e0f
rizky fenaldo <rizkyfenaldoo@gmail.com>
2017-03-30 21:23:51 +0700

    Revert "decrypt masih error, insyaAllah gak ganggu encrypt"
    This reverts commit 207e4b50476d250c8fc3fc731f97ff0e075e3014.

--- des_utils.py
+++ des_utils.py
@@ -1,24 +1,8 @@
 
-binary_list = []
-bin_to_text_dict = {}
+hex_dict = {'0000': '0', '0001': '1', '0010': '2', '0011': '3', '0100': '4', '0101': '5', '0110': '6', '0111': '7',
+          '1000': '8', '1001': '9', '1010': 'a', '1011': 'b', '1100': 'c', '1101': 'd', '1110': 'e', '1111': 'f'}
 
-# for calculation of 8-bit list for every character
-for n in range(256):
-    b = [0, 0, 0, 0, 0, 0, 0, 0]
-    for i in range(0, 8):
-        if n % 2:
-            b[7 - i] = 1
-        n = n // 2
-    binary_list.append(b)
-
-# for calculation of char from string of 8-bits
-k = 0
-for i in binary_list:
-    string = ''
-    for j in i:
-        string += str(j)
-    bin_to_text_dict[string] = chr(k)
-    k += 1
+hex_revesred_dict = {v: k for k, v in hex_dict.items()}
 
 def string_to_array(text, length):
     texts = []
@@ -36,7 +20,7 @@ def string_to_binary(text):
     return binary
 
 def hex_to_binary(hex):
-    return bin(int(hex, 16))[2:].zfill(4)
+    return hex_revesred_dict[hex]
 
 def int_to_binary(num):
     return format(num, 'b').zfill(4)
@@ -45,8 +29,6 @@ def int_to_binary(num):
 def binary_to_int(binary):
     return int(binary, 2)
 
-def binary_to_text(binary):
-    return bin_to_text_dict[binary]
 
 def binary_to_hex(binary):
     return hex(int(binary, 2))[2:].zfill(2)
@@ -80,3 +62,13 @@ def debug(tag, content, length=None):
         print tag + ' : ' + content
     else:
         print tag + ' : ' + split_string(content, length)
+
+def add_pads_if_necessary(s):
+    """adding bits to make an integer number of 64-bit blocks
+    """
+    number_of_vacancy = len(s) % 64
+    need_pads = number_of_vacancy > 0
+    if need_pads:
+        for i in range(64 - number_of_vacancy):
+            s.append(0)
+    return s
